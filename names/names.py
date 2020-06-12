5a9a96e3e83473e65f076054976bf5de0bbe9044
fuston05 <fuston05@yahoo.com>
2020-06-12 16:12:23 -0400

    passes tests on names.py, cuts runtime down to 0.01 sec, O(Log n)

--- names/names.py
+++ names/names.py
@@ -1,5 +1,5 @@
 import time
-
+from binary_search_tree import BSTNode
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,17 +10,35 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# build names list 1: **** O(n) ****
+count = 0
+for n in names_1:
+    if count == 0:
+        list_1_names = BSTNode(n)
+        count += 1
+    else:
+        list_1_names.insert(n)
+        count += 1
+
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# **** runtime: O(n^C), quadric (11.5 on my machine) ****
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# **** O(Log n) ****
+for n in names_2:
+    if list_1_names.contains(n):
+        duplicates.append(n)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem

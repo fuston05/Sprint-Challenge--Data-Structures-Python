ebecac5e569a00f96d7c2848975e0ccfd96500a8
fuston05 <fuston05@yahoo.com>
2020-06-12 12:38:10 -0400

    passes ringBuffer.py tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,19 +1,46 @@
 from doubly_linked_list import DoublyLinkedList
 
+
 class RingBuffer:
     def __init__(self, capacity):
-        self.capacity= 1000
-        self.storage= DoublyLinkedList()
+        self.capacity = capacity
+        self.storage = DoublyLinkedList()
+        self.lastOverwrittenNode = None
 
     def append(self, item):
         # check cap
         if len(self.storage) < self.capacity:
+            # if less than cap, add to end
             self.storage.add_to_tail(item)
         else:
-            # remove old head
-            self.storage.remove_from_head()
-            # add new node as the new head
-            self.storage.add_to_head(item)
+            if len(self.storage) == self.capacity:
+                # if cap is full, but nothing new has been added yet
+                if self.lastOverwrittenNode == None:
+                    self.storage.remove_from_head()
+                    self.storage.add_to_head(item)
+                    # set lastOverwrittenNode
+                    self.lastOverwrittenNode = self.storage.head
+                # cap == full, things have been added
+                else:
+                    # if tail was last one to be overwritten
+                    if self.lastOverwrittenNode == self.storage.tail:
+                        # set head to be next to get overwritten
+                        self.lastOverwrittenNode = self.storage.head
+                        self.lastOverwrittenNode.value = item
+                    else:
+                        # if tail was not last to be overwritten,
+                        # overwrite "next"
+                        self.lastOverwrittenNode = self.lastOverwrittenNode.next
+                        self.lastOverwrittenNode.value = item
 
     def get(self):
-        pass
+        # return all elements values that are not None
+        # returns as a list in given order
+        data_list = []
+        currentNode = self.storage.head
+
+        while currentNode is not None:
+            if currentNode.value is not None:
+                data_list.append(currentNode.value)
+                currentNode = currentNode.next
+        return data_list

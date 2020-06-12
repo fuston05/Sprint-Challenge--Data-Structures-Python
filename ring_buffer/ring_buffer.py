0fd59f9bbdb4172fe107226296fb3d43c29ca80e
fuston05 <fuston05@yahoo.com>
2020-06-12 11:27:24 -0400

    imports doubly_linked_list as storage, creates method 'append' to RingBuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
+from doubly_linked_list import DoublyLinkedList
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity= 1000
+        self.storage= DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # check cap
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+        else:
+            # remove old head
+            self.storage.remove_from_head()
+            # add new node as the new head
+            self.storage.add_to_head(item)
 
     def get(self):
         pass
